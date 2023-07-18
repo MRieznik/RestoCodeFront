a0ca521b9f8beba19ed6f11c7f2ce98ab69b076e
Marcos Rieznik <m.rieznik17@gmail.com>
2023-07-18 19:17:15 -0300

    Modificando texto del error 404

--- resto-code-front/src/Pages/error404/error404.jsx
+++ resto-code-front/src/Pages/error404/error404.jsx
@@ -5,10 +5,10 @@ import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 const Error404 = () => {
   return (
     <body className="error404 ">
-      <div class="d-flex align-items-center justify-content-center ">
+      <div class="d-flex align-items-center justify-content-center  ">
         
-          <div className="imagen404 "></div>
-          <div className="text-center position-absolute mt-3 m">
+          <div className="imagen404" ></div>
+          <div className="text-center position-absolute mt-3">
             <h2 class="display-1 fw-bold text-white texto404 ">404</h2>
             <p class="fs-3 text-white fw-bold">
               <span class="text-danger fw-bold">Opps!</span> Página no encontrada.
@@ -16,7 +16,7 @@ const Error404 = () => {
             <p class="text-white">
               Lo siento, la página que estás buscando no se pudo encontrar.
             </p>
-            <a href="" className="btn boton-error">
+            <a href="" className="btn boton-error" alt="boton de volver a inicio">
               Volver a la página principal
             </a>
         </div>
