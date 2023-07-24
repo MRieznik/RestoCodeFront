f4c25ef7b71c9cd906624e27be2b0ba3c66539e6
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-07-24 18:48:34 -0300

    Modificacion de imagenes

--- resto-code-front/src/Components/CONTENIDO ABOUT/ContenidoAbout.jsx
+++ resto-code-front/src/Components/CONTENIDO ABOUT/ContenidoAbout.jsx
@@ -1,5 +1,5 @@
 import "./ContenidoAbout.css";
-
+import fotolautaro from "../../Image/ABOUT-IMAGE/fotoLuisina.jpg"
 const ContenidoAbout = () => {
   return (
     <>
@@ -8,9 +8,121 @@ const ContenidoAbout = () => {
           <div className="sombraPortadaAbout">
             <h1>Resto code</h1>
             <h3>NUESTRA REVOLUCIÓN ES EL COMIENZO DE LA EVOLUCIÓN</h3>
-            <h5><strong>Resto Code</strong> nace como fruto del compañerismo y la pasión por lo que hacemos.</h5>
+            <h5>
+              <strong>Resto Code</strong> nace como fruto del compañerismo y la
+              pasión por lo que hacemos.
+            </h5>
           </div>
         </div>
+        <div className="bloqueUnoDeABout">
+          <div className="cajaInfoMasImagenAbout">
+            <h3>
+              SOMOS APASIONADOS
+            </h3>
+            <p>
+              <strong>En todo lo que hacemos, desde el primer día.</strong>
+            </p>
+            <p>
+              En un rincón de Orihuela, Alicante, cuando aún la cerveza
+              artesanal era un sueño muy lejano en todo el mundo.
+            </p>
+          </div>
+          <div className="cajaFotosIntegrantes">
+            <img src={fotolautaro} alt="" />
+          </div>
+          <div className="cajaInformacionIntegrantes"></div>
+        </div>
+        <div className="bloqueUnoDeABout">
+          <div className="cajaInfoMasImagenAbout">
+            <h3>
+              SOMOS APASIONADOS
+            </h3>
+            <p>
+              <strong>En todo lo que hacemos, desde el primer día.</strong>
+            </p>
+            <p>
+              En un rincón de Orihuela, Alicante, cuando aún la cerveza
+              artesanal era un sueño muy lejano en todo el mundo.
+            </p>
+          </div>
+          <div className="cajaFotosIntegrantes">
+            <img src={fotolautaro} alt="" />
+          </div>
+          <div className="cajaInformacionIntegrantes"></div>
+        </div>
+        <div className="bloqueUnoDeABout">
+          <div className="cajaInfoMasImagenAbout">
+            <h3>
+              SOMOS APASIONADOS
+            </h3>
+            <p>
+              <strong>En todo lo que hacemos, desde el primer día.</strong>
+            </p>
+            <p>
+              En un rincón de Orihuela, Alicante, cuando aún la cerveza
+              artesanal era un sueño muy lejano en todo el mundo.
+            </p>
+          </div>
+          <div className="cajaFotosIntegrantes">
+            <img src={fotolautaro} alt="" />
+          </div>
+          <div className="cajaInformacionIntegrantes"></div>
+        </div>
+        <div className="bloqueUnoDeABout">
+          <div className="cajaInfoMasImagenAbout">
+            <h3>
+              SOMOS APASIONADOS
+            </h3>
+            <p>
+              <strong>En todo lo que hacemos, desde el primer día.</strong>
+            </p>
+            <p>
+              En un rincón de Orihuela, Alicante, cuando aún la cerveza
+              artesanal era un sueño muy lejano en todo el mundo.
+            </p>
+          </div>
+          <div className="cajaFotosIntegrantes">
+            <img src={fotolautaro} alt="" />
+          </div>
+          <div className="cajaInformacionIntegrantes"></div>
+        </div>
+        <div className="bloqueUnoDeABout">
+          <div className="cajaInfoMasImagenAbout">
+            <h3>
+              SOMOS APASIONADOS
+            </h3>
+            <p>
+              <strong>En todo lo que hacemos, desde el primer día.</strong>
+            </p>
+            <p>
+              En un rincón de Orihuela, Alicante, cuando aún la cerveza
+              artesanal era un sueño muy lejano en todo el mundo.
+            </p>
+          </div>
+          <div className="cajaFotosIntegrantes">
+            <img src={fotolautaro} alt="" />
+          </div>
+          <div className="cajaInformacionIntegrantes"></div>
+        </div>
+        <div className="bloqueUnoDeABout">
+          <div className="cajaInfoMasImagenAbout">
+            <h3>
+              SOMOS APASIONADOS
+            </h3>
+            <p>
+              <strong>En todo lo que hacemos, desde el primer día.</strong>
+            </p>
+            <p>
+              En un rincón de Orihuela, Alicante, cuando aún la cerveza
+              artesanal era un sueño muy lejano en todo el mundo.
+            </p>
+          </div>
+          <div className="cajaFotosIntegrantes">
+            <img src={fotolautaro} alt="" />
+          </div>
+          <div className="cajaInformacionIntegrantes"></div>
+        </div>
+        
       </main>
     </>
   );
