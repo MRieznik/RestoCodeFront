f270c2e72cdbf4df657f616e0bf68ac972d06c5b
Luisina Martinez <luuumartinez13@gmail.com>
2023-07-25 11:12:09 -0300

    diseño con estilo de la pag

--- resto-code-front/src/Pages/Reservas/reservas.jsx
+++ resto-code-front/src/Pages/Reservas/reservas.jsx
@@ -8,96 +8,133 @@ import {
   Col,
   Button,
   CardImg,
+  CardGroup,
 } from "react-bootstrap/";
+// import imagereserva from "../../Image/RESERVA-IMAGE/imgReserva.jpeg"
 import "./reservas.css";
 
 const Reservas = () => {
   return (
     <>
-      {/* <div className="pageReserva d-flex align-items-center justify-content-center container-fluid ">
-        <div className="card cardReserva ">
-          <div className="card-img" variant="top" src="" />
-          <div className="card-body">
-            <div className="card-title">
-              <h1>RESERVAS</h1>
-            </div>
-            <div className="card-text">
-              <InputGroup className="mb-3">
-                <Form.Control
-                  placeholder="Recipient's username"
-                  aria-label="Recipient's username"
-                />
-                <InputGroup.Text id="basic-addon2">
-                  @example.com
-                </InputGroup.Text>
-              </InputGroup>
-              <InputGroup size="sm">
-                <DropdownButton
-                  variant="outline-secondary"
-                  title="Dropdown"
-                  id="input-group-dropdown-1"
-                >
-                  <Dropdown.Item href="#">Action</Dropdown.Item>
-                  <Dropdown.Item href="#">Another action</Dropdown.Item>
-                  <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="#">Separated link</Dropdown.Item>
-                </DropdownButton>
-                <Form.Control aria-label="Text input with dropdown button" />
-              </InputGroup>{" "}
-              <br></br>
-              <InputGroup>
-                <InputGroup.Text>With textarea</InputGroup.Text>
-                <Form.Control as="textarea" aria-label="With textarea" />
-              </InputGroup>
-              <br></br>
-              <button className="btn botonReserva">Go somewhere</button>
-            </div>
-          </div>
+    <main className="contenedorPrincipalReserva">
+      <div className="contenedorFormularioReserva">
+        <div className="tituloFormReserva">
+          <h2>No esperes en la fila</h2>
+          <h4>¡Reserva y entrá directo a la diversión!</h4>
         </div>
-      </div> */}
-
-      <Container className="pageReserva" fluid>
-        <Row>
-          <Col>
-            <Card className="cardReserva">
-                <Col>
-                  <img
-                    className="imgReserva"
-                    src="../src/Image/restocodeimg.jpeg"
-                    alt=""
+      <Form className="formularioReserva">
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="labelReservas">
+                    Fecha de reserva
+                  </Form.Label>
+                  <Form.Control
+                    className="inputReservas"
+                    type="date"
+                    placeholder="Ingrese su nombre de usuario"
                   />
-                </Col>
-              <Card.Body>
-                <Card.Title className="tituloCardReservas">
-                  <h1>RESERVAS</h1>
-                </Card.Title>
-                <Form>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlInput1"
-                  >
-                    <Form.Label className="labelReservas">Nombre de usuario</Form.Label>
-                    <Form.Control className="inputReservas" type="text" placeholder="Ingrese su nombre de usuario" />
-                  </Form.Group>
-                  <Form.Label className="labelReservas">Cantidad de comensales</Form.Label>
-                  <Form.Select className="inputReservas" aria-label="Default select example">
-                    <option>...</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                  </Form.Select>
-                  <p></p>
-                  <Form.Label className="labelReservas">Comentarios</Form.Label>
-                  <Form.Control className="inputReservas" as="textarea" aria-label="With textarea" />
-                  <p></p>
-                  <Button className="botonReserva">Confirmar</Button>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="labelReservas">
+                    Hora de reserva
+                  </Form.Label>
+                  <Form.Control
+                    className="inputReservas"
+                    type="time"
+                    placeholder="Ingrese su nombre de usuario"
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="labelReservas">
+                    Cantidad de invitados
+                  </Form.Label>
+                  <Form.Control
+                    className="inputReservas"
+                    type="number"
+                    placeholder="Ingrese numero de invitados"
+                  />
+                </Form.Group>
+                <Form.Label className="labelReservas">¿Qué debemos saber sobre tu evento?</Form.Label>
+                <Form.Control
+                  className="infoEvento"
+                  type="text"
+                  // aria-label="With textarea"
+                />
+                <Button className="botonReserva">Confirmar</Button>
+              </Form>
+
+      </div>
+      <div className="contenedorImgReserva">
+        {/* <img src={imagereserva} className="imageReserva"></img> */}
+      </div>
+    </main>
+
+
+      {/* <Container fluid className="pageReserva">
+        <Row>
+        <Col lg={6}>
+          <Card className="cardReserva">
+            <Card.Body>
+              <Card.Title className="tituloCardReservas">
+                <h1>RESERVAS</h1>
+              </Card.Title>
+              <Form>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="labelReservas">
+                    Nombre de usuario
+                  </Form.Label>
+                  <Form.Control
+                    className="inputReservas"
+                    type="text"
+                    placeholder="Ingrese su nombre de usuario"
+                  />
+                </Form.Group>
+                <Form.Label className="labelReservas">
+                  Cantidad de comensales
+                </Form.Label>
+                <Form.Select
+                  className="inputReservas"
+                  aria-label="Default select example"
+                >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </Form.Select>
+                <p></p>
+                <Form.Label className="labelReservas">Comentarios</Form.Label>
+                <Form.Control
+                  className="inputReservas"
+                  as="textarea"
+                  aria-label="With textarea"
+                />
+                <p></p>
+                <Button className="botonReserva">Confirmar</Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col lg={6}>
+          <div className="contenedorImgReserva">
+          <img
+            className="imgReserva"
+            src="../src/Image/restocodeimg.jpeg"
+            alt=""
+          />
+          </div>
+        </Col>
         </Row>
-      </Container>
+      </Container> */}
     </>
   );
 };
