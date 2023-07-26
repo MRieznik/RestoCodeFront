b30ad3d7390800be5a7ac857eca5592ab2f7b2b6
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-07-25 23:42:08 -0300

    borre bloque 3

--- resto-code-front/src/Components/CONTENIDO GALERIA/ContenidoGaleria.jsx
+++ resto-code-front/src/Components/CONTENIDO GALERIA/ContenidoGaleria.jsx
@@ -1,7 +1,6 @@
 import "../CONTENIDO GALERIA/ContenidoGaleria.css";
 import videoextreme from "../../Image/GALERIA-IMAGE/videoPrueba.mp4";
 import videocerveza from "../../Image/GALERIA-IMAGE/videocerveza.mp4";
-import videobloquecervezas from "../../Image/GALERIA-IMAGE/videoBloqueCervezas.mp4";
 
 const Galeria = () => {
   return (
@@ -47,76 +46,9 @@ const Galeria = () => {
             <h4>Para acompañar tus comidas</h4>
           </div>
         </div>
-
-        {/* bloque 3 */}
-
-        <div className="presentacionCervezas">
-          <div className="contenedorBloqueCervezas">
-            <div className="contenedorFraseCerveza">
-              <h2>Ven a Probar Nuestras Cervezas y Cocteles</h2>
-              <h4>¡Solo lo mejor para vos!</h4>
-            </div>
-            <video
-              className="videoBloqueCervezas"
-              src={videobloquecervezas}
-              autoPlay
-              loop
-              muted
-            ></video>
-          </div>
-        </div>
-
-        <div className="presentacionCervezas2">
-          <div className="contenedorFraseCerveza2">
-            <h2>Las Mejores Cervezas esta aqui</h2>
-            <h4>Para con quien quieras compartirlas !</h4>
-          </div>
-
-          <div className="contenedorImagenVariado2">
-            <div className="sombraPersonaCerveza"></div>
-          </div>
-        </div>
       </main>
     </>
   );
 };
 
-<<<<<<< HEAD
 export default Galeria;
-=======
-                {/* bloque 3 */}
-{/* 
-                <div className="presentacionCervezas">
-                    <div className="contenedorBloqueCervezas">
-                        <div className="contenedorFraseCerveza">
-                            <h2>Ven a Probar Nuestras Cervezas y Cocteles</h2>
-                            <h4>¡Solo lo mejor para vos!</h4>
-                        </div>
-                        <video className="videoBloqueCervezas" src={videobloquecervezas}
-                            autoPlay
-                            loop
-                            muted>
-                        </video>
-                    </div>
-                </div>
-
-
-                <div className="presentacionCervezas2">
-                    <div className="contenedorFraseCerveza2">
-                        <h2>Las Mejores Cervezas esta aqui</h2>
-                        <h4>Para con quien quieras compartirlas !</h4>
-                    </div>
-
-                <div className="contenedorImagenVariado2">
-                    <div className="sombraPersonaCerveza">
-                    </div>
-                </div>
-                </div> */}
-            </main>
-
-        </>
-    )
-}
-
-export default Galeria
->>>>>>> 1e56dcac8cc35b71a54918d1fc3e3e21e90d0cb6
