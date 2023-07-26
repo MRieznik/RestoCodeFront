e05103b476b498a2866f0689c6f4936deaab95a8
Marcos Rieznik <m.rieznik17@gmail.com>
2023-07-26 19:10:00 -0300

    Cambiando diseno del error 404

--- resto-code-front/src/Pages/error404/error404.jsx
+++ resto-code-front/src/Pages/error404/error404.jsx
@@ -1,14 +1,17 @@
 import "./error404.css";
+import imagenHamburugesaMordida from "../../Image/ERROR404/hamburguesaMordida.png"
+import homero404 from "../../Image/ERROR404/homeroError404.png"
 import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+
 
 const Error404 = () => {
   return (
     <>
     <div className="error404">
-      <div className="d-flex align-items-center justify-content-center  container-fluid ">
-          <div className="imagen404  " ></div>
+      <div className="d-flex align-items-center justify-content-center container-fluid ">
+
           <div className="text-center position-absolute mt-2">
-            <h2 className="display-1 fw-bold text-white texto404 ">404</h2>
+            <h1 className="display-1 fw-bold text-white texto404 ">4<img src={imagenHamburugesaMordida} alt="Imagen de hamburguesa mordida" className="imagen404 " />4</h1>
             <p className="fs-3 text-white fw-bold">
               <span className="text-danger fw-bold">Opps!</span> Página no encontrada.
             </p>
