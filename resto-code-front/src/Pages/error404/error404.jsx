6b1a2a27518ca3ee312cbd31c7ff26dbdb521574
Marcos Rieznik <m.rieznik17@gmail.com>
2023-07-26 19:53:33 -0300

    Dando estilos nuevos al 404

--- resto-code-front/src/Pages/error404/error404.jsx
+++ resto-code-front/src/Pages/error404/error404.jsx
@@ -1,29 +1,28 @@
 import "./error404.css";
-import imagenHamburugesaMordida from "../../Image/ERROR404/hamburguesaMordida.png"
-import homero404 from "../../Image/ERROR404/homeroError404.png"
+import imagenHamburugesaMordida from "../../Image/ERROR404/hamburguesaMordida.png";
+import homero404 from "../../Image/ERROR404/homeroError404.png";
 import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
-
 
 const Error404 = () => {
   return (
     <>
-    <div className="error404">
-      <div className="d-flex align-items-center justify-content-center container-fluid ">
-
-          <div className="text-center position-absolute mt-2">
-            <h1 className="display-1 fw-bold text-white texto404 ">4<img src={imagenHamburugesaMordida} alt="Imagen de hamburguesa mordida" className="imagen404 " />4</h1>
-            <p className="fs-3 text-white fw-bold">
-              <span className="text-danger fw-bold">Opps!</span> Página no encontrada.
-            </p>
-            <p className="text-white">
-              Lo siento, la página que estás buscando no se pudo encontrar.
-            </p>
-            <a href="" className="btn boton-error" alt="boton de volver a inicio">
-              Volver a la página principal
-            </a>
+      <main className="contenedorPrincipalError">
+        <div className="contenedor404">
+          <div className="contenedorHomero">
+            <img src={homero404} alt="Homero quejandose" />
+          </div>
+          <div className="letras404">
+            <div className="contenedorLetras404">
+              <p className="cuatro">4</p>{" "}
+              <img src={imagenHamburugesaMordida} alt="Hamburguesa mordida" />{" "}
+              <p className="cuatro">4</p>
+            </div>
+            <div className="contenedorBoton">
+              <button className="boton-error">Volver al inicio</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </main>
     </>
   );
 };
