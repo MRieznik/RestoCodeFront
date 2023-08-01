695ad86c48ccfe6bae6e0e819f0930c15eb4698f
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-07-31 23:05:15 -0300

    agrego estilos y menu para user-icon

--- resto-code-front/src/Components/HEADER/Header.jsx
+++ resto-code-front/src/Components/HEADER/Header.jsx
@@ -1,20 +1,35 @@
-import { Container, Nav, Navbar, Modal } from "react-bootstrap";
+import { Container, Nav, Navbar, Modal, Dropdown } from "react-bootstrap";
 import { useState, useContext } from "react";
 import { UsuariosContext } from "../../context/UsersContext";
 import logo from "../../Image/HEADER-IMAGE/restoCodeLogo.png";
 import ModalInicarSesion from "../MODAL INICAR-SESION/ModalInicarSesion";
+import ModalUserReservas from "../USER-ICON/ModalUserReservas";
+import UserIcon from "../USER-ICON/UserIcon";
 import "./Header.css";
 
+
 const Header = () => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+
+  const [showReservas, setReservas] = useState(false);
+  const handleCloseReservas = () => setReservas(false);
+  const handleShowReservas = () => setReservas(true);
 
   const iniciarSesion = () => {
-    handleShow();
+    handleShowModal();
+  };
+
+  const tusReservas = () => {
+    handleShowReservas();
   };
   const { logOut } = useContext(UsuariosContext);
   const usuarioLogueado = JSON.parse(localStorage.getItem("user"));
+
+ const iniciales = usuarioLogueado ? usuarioLogueado.nombre.split("")[0]+usuarioLogueado.apellido.split("")[0] : console.log("No hay usuario logueado.")
+  
+
 
   return (
     <>
@@ -86,26 +101,51 @@ const Header = () => {
                     <></>
                   )}
                 </Nav>
-                <div className="contenedor-iniciarSesion-registrarse">
-                  <Nav.Link href="#link" className="NavLink" onClick={logOut}>
-                    Cerrar sesión
-                  </Nav.Link>
-                </div>
+                <Nav className="contenedor-iniciarSesion-registrarse justify-content-end">                  
+                    <Dropdown  className="desplegable">
+                      <Dropdown.Toggle
+                        id="dropdown-basic"
+                        className="user-icon"
+                      >
+                        <UserIcon iniciales={iniciales} />
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu className="dropMenu" >
+                        <Dropdown.Item href="#/action-1" onClick={tusReservas}>
+                          Mis Reservas
+                        </Dropdown.Item>                      
+                        <Dropdown.Item href="#/action-2" onClick={logOut} >
+                         Cerrar Sesión
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                    <Modal
+                      show={showReservas}
+                      onHide={handleCloseReservas}
+                      className="ventanaModalInicarSesion"
+                    >
+                      <Modal.Header closeButton className="headerModal">
+                        <Modal.Title>Tus Reservas</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body className="bodyModal">
+                        <ModalUserReservas handleClose={handleCloseReservas} />
+                      </Modal.Body>
+                    </Modal>                  
+                </Nav>
               </Navbar.Collapse>
             </>
           )}
         </Container>
       </Navbar>
       <Modal
-        show={show}
-        onHide={handleClose}
+        show={showModal}
+        onHide={handleCloseModal}
         className="ventanaModalInicarSesion"
       >
         <Modal.Header closeButton className="headerModal">
           <Modal.Title>Ingresa tu usuario</Modal.Title>
         </Modal.Header>
         <Modal.Body className="bodyModal">
-          <ModalInicarSesion handleClose={handleClose} />
+          <ModalInicarSesion handleClose={handleCloseModal} />
         </Modal.Body>
       </Modal>
     </>
