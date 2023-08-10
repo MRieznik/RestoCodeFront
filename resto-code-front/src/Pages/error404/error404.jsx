a0513237de02ede39f62083d0f121f26cb362038
Marcos Rieznik <m.rieznik17@gmail.com>
2023-08-10 09:21:12 -0300

    Nuevo error 404 con hamburguesa giratoria

--- resto-code-front/src/Pages/error404/error404.jsx
+++ resto-code-front/src/Pages/error404/error404.jsx
@@ -1,6 +1,5 @@
 import "./error404.css";
 import imagenHamburugesaMordida from "../../Image/ERROR404/hamburguesaMordida.png";
-import homero404 from "../../Image/ERROR404/homeroError404.png";
 import { useNavigate } from "react-router-dom";
 
 const Error404 = () => {
@@ -8,32 +7,37 @@ const Error404 = () => {
 
   return (
     <>
-      <main className="contenedorPrincipalError">
-        <div className="contenedor404">
-          <div className="contenedorHomero">
-            <img src={homero404} alt="Homero quejandose" />
+      <main className="contenedorError">
+        <div className="contenedor">
+          <div className="four">
+            <h2>4</h2>
           </div>
-          <div className="letras404">
-            <div className="contenedorLetras404">
-              <p className="cuatro">4</p>{" "}
-              <img src={imagenHamburugesaMordida} alt="Hamburguesa mordida" />{" "}
-              <p className="cuatro">4</p>
-            </div>
-            <div className="contenedorBoton">
+
+          <div className="circle">
+            <img
+              src={imagenHamburugesaMordida}
+              alt="Imagen mordida de hamburguesa"
+              srcset=""
+            />
+          </div>
+
+          <div className="four">
+            <h2>4</h2>
+          </div>
+          <div className="desc">
+            <h4>PAGINA NO ENCONTRADA</h4>{" "}
               <button
+                className="button-arounder"
                 onClick={() => {
                   navigation("/");
                 }}
-                className="boton-error"
               >
+                {" "}
                 Volver al inicio
               </button>
-            </div>
           </div>
         </div>
-        <div className="contenedorTextoError">
-          <h2>¡Se produjo un error!</h2>
-        </div>
+        <div className="buttons-container"></div>
       </main>
     </>
   );
